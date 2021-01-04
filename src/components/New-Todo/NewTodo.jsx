1c18a16d1c8387d5275c9999a630d9f43d8a0b40
Zuzki <ldegrac071@gmail.com>
2021-01-04 10:30:06 -0500

    Finished styling todo input

--- src/components/New-Todo/NewTodo.jsx
+++ src/components/New-Todo/NewTodo.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import RoundButtons from '../UI/RoundButtons'
 import { NewContainer, NewInput } from './NewTodoStyled'
 
 function NewTodo() {
   return (
     <NewContainer >
+      <RoundButtons />
       <NewInput type="text" placeholder="Create a new todo..." />
     </NewContainer>
   )
--- src/components/New-Todo/NewTodoStyled.jsx
+++ src/components/New-Todo/NewTodoStyled.jsx
@@ -1,10 +1,22 @@
 import styled from 'styled-components'
 
 export const NewContainer = styled.div`
+  background-color: ${ ({ theme }) => { return theme.colors.VeryDarkDesaturatedBlue} };
   width: 100%;
-  border: 1px solid black;
   margin-top: 25px;
-  padding: 5px 10px;
+  padding: 15px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
 `;
 
-export const NewInput = styled.input``;
+export const NewInput = styled.input`
+  background: none;
+  border: none;
+  margin-left: 10px;
+  color: hsl(0, 0%, 40%);
+
+  &:focus{
+    outline: none;
+  }
+`;
